0235517e36e8dbe77c1af15c42098db1201a4e3c
thTs0ustas <60882323+thTs0ustas@users.noreply.github.com>
2020-09-20 19:42:37 +0300

    added laps

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import useApp from './useApp.js'
 import './App.css';
 
 
-
-
 function App() {
-
-  const {seconds,minutes,title,isRunning,startTimer,stopTimer,resetTimer} = useApp()
-
+  
+  const {seconds, minutes, title, isRunning,lapsRemaining, startTimer, stopTimer, resetTimer} = useApp()
+  
   return (
-     <div className="App">
-       <h2>{title}</h2>
-       <div className="timer">
-         <span>{minutes}</span>
-         <span>:</span>
-         <span>{seconds}</span>
-       </div>
-       <div className="buttons">
-         {!isRunning || <Button onClick={()=>startTimer()} color="secondary" variant="contained">Start</Button>}
-         {isRunning || <Button onClick={()=>stopTimer()} color="primary" variant="contained">Stop</Button>}
-         <Button onClick={resetTimer} color="primary" variant="contained">Reset</Button>
-       </div>
-     </div>
+    <div className="App">
+      <h2>{title}</h2>
+      <div className="laps">
+        <span>Laps</span>
+        <span>: </span>
+        <span>{lapsRemaining}</span>
+      </div>
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      <div className="buttons">
+        {!isRunning || <Button onClick={() => startTimer(lapsRemaining)} color="secondary" variant="contained">Start</Button>}
+        {isRunning || <Button onClick={() => stopTimer()} color="primary" variant="contained">Stop</Button>}
+        <Button onClick={resetTimer} color="primary" variant="contained">Reset</Button>
+      </div>
+    </div>
   );
 }
 
--- src/useApp.js
+++ src/useApp.js
@@ -3,24 +3,36 @@ import React, {useRef, useState} from 'react';
 
 const useApp = () => {
   const pad_time = (time) => time.toString().padStart(2, '0')
+  const settingLaps = 2
   
   const [title, setTitle] = useState('Start the countdown!!!')
-  const [timeLeft, setTimeLeft] = useState(10)
-  const [isRunning , setIsRunning] = useState(true)
+  const [timeLeft, setTimeLeft] = useState(2)
+  const [isRunning, setIsRunning] = useState(true)
+  const [laps, setLaps] = useState(settingLaps)
+  const [rest, setRest] = useState(laps?laps-1:0)
+  const [timeToRest, setTimeToRest] = useState(false)
   
   const minutes = pad_time(Math.floor(timeLeft / 60))
   const seconds = pad_time(timeLeft - minutes * 60)
+  const lapsRemaining = pad_time(laps)
   const interval = useRef(null)
   
-  const startTimer = () => {
+  const startTimer = (laps) => {
     setIsRunning(false)
     setTitle('Time is passing')
     if (interval.current !== null) return;
-    
     interval.current = setInterval(() => {
       setTimeLeft(timeLeft => {
-        if (timeLeft >=1) return timeLeft - 1
+        if (timeLeft >= 1) return timeLeft - 1
         resetTimer()
+        if (laps>0) {
+          setLaps(laps-1)
+          startTimer(laps-1)
+        }
+        if (laps===0) {
+          resetTimer()
+          setLaps(settingLaps)
+        }
         return 0
       })
     }, 1000)
@@ -42,8 +54,13 @@ const useApp = () => {
     setTimeLeft(10)
     setTitle('Ready for another one')
     setIsRunning(true)
+    
   }
-  return {seconds, minutes, title,isRunning, startTimer, stopTimer, resetTimer}
+  
+  
+  
+  
+  return {seconds, minutes, title, isRunning, lapsRemaining, startTimer, stopTimer, resetTimer}
 }
 
 export default useApp
